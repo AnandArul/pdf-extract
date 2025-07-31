4687d4dca021df892d42c2a122e86dcbc434476e
AnandArul <anandarul6@gmail.com>
2025-07-31 07:16:15 +0530

    PDF extract using pdfplumber

--- upload.py
+++ upload.py
@@ -1,8 +1,6 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, PhotoImage
 from utilities.upload_handler import upload_file
-
-
 
 def create_main_window():
     root=tk.Tk()
@@ -11,25 +9,6 @@ def create_main_window():
     
     root.grid_rowconfigure(0, weight=0)
     root.grid_columnconfigure(0, weight=1)
-    
-    # --------- Style Configuration for White Button ---------
-    # style = ttk.Style()
-    # style.theme_use('default')  # Use default theme for better styling control
-
-    # style.configure("White.TButton",
-    #     background="white",
-    #     foreground="black",
-    #     borderwidth=1,
-    #     padding=6,
-    #     font=('Arial', 10, 'bold'),
-    #     relief="solid"
-    # )
-    # style.map("White.TButton",
-    #     background=[('active', '#f0f0f0')],
-    #     relief=[('pressed', 'sunken'), ('!pressed', 'solid')],
-    #     bordercolor=[('!active', '#e1e1e1'), ('active', '#999')]
-    # )
-
     
     # --------title frame config--------
     header_frame=tk.Frame(root,bg='white', borderwidth=1, height=50, padx=10, pady=10, relief='ridge')
@@ -40,24 +19,24 @@ def create_main_window():
     # --------configure titles inside frame--------
     header_label=tk.Label(header_frame, text="Extract Pdf Data", bg='white', font=('Arial', 18, 'bold'))
     header_label.grid(row=0, column=0, sticky="w")
-    # header_label.pack(anchor='center')      #center alignment option 1
-    # header_label.pack(expand=True)      #center alignment option 2
     
     # --------configure close button inside frame--------
-    close_button=tk.Button(header_frame, text="Close", command=root.destroy, bg='white', relief='solid', highlightbackground="#e1e1e1", highlightthickness=1)
+    close_button=tk.Button(header_frame, 
+                   text="Close", 
+                   command=root.destroy,
+                   activeforeground="white",
+                   anchor="center",
+                   bd=3,
+                   bg="lightgray",
+                   disabledforeground="gray",
+                   fg="black",
+                   font=("Arial", 12),
+                   highlightbackground="white",
+                   highlightcolor="green",
+                   highlightthickness=2,
+                   justify="center",
+                   overrelief="raised")
     close_button.grid(row=0, column=1, sticky="e")
-    
-    # --------- ttk.Button on Right ---------
-    # header_frame.grid_columnconfigure(0, weight=1)
-    # header_frame.grid_columnconfigure(1, weight=0)
-    # close_button = ttk.Button(
-    #     header_frame,
-    #     text="Close",
-    #     style="White.TButton",
-    #     command=root.destroy
-    # )
-    # close_button.grid(row=0, column=1, sticky="e", padx=10)
-    
     
     # --------configure frame for upload button--------
     upload_frame=tk.Frame(root,bg='white', borderwidth=1, padx=10, pady=20, relief='ridge')
@@ -66,8 +45,22 @@ def create_main_window():
     # header_frame.grid_propagate(False)              # sets fixed height
     
     # --------configure upload button inside frame--------
-    upload_button=tk.Button(upload_frame, text='Upload', command=lambda:upload_file(data_frame,footer_frame))
-    upload_button.grid(row=1,column=1, sticky="ew")
+    upload_button=tk.Button(
+        upload_frame,
+        text="Upload File",
+        command=lambda:upload_file(data_frame,footer_frame),
+        bg="#0078D7",
+        fg="black",
+        highlightbackground="white",
+        font=("Arial", 11, "bold"),
+        relief="flat",
+        padx=10,
+        pady=5
+    )
+    upload_button.pack()
+    
+    upload_desc=tk.Label(upload_frame, text="(upload pdf files only)", bg='white', font=('Arial', 10, 'italic'))
+    upload_desc.pack()
     
     # --------configure frame for extracted data--------
     root.grid_rowconfigure(3, weight=1)
@@ -77,12 +70,6 @@ def create_main_window():
     # --------footer data--------
     footer_frame=tk.Frame(root, bg='white', padx=10, pady=10, relief='ridge')
     footer_frame.grid(row=4,column=0, sticky='ew')
-    
-    
-    
-    
-    
-    
     
     root.mainloop()
 
